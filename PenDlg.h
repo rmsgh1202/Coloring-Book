8111c95278e7c73332b1844002a7430ca2d42277
rmsgh1202 <54826629+rmsgh1202@users.noreply.github.com>
2019-12-17 19:51:33 +0900

    Add files via upload

---
+++ PenDlg.h
@@ -0,0 +1,40 @@
+﻿
+// PenDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CPenDlg 대화 상자
+class CPenDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	COLORREF Col;
+	CPenDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+	CImage img;
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_PEN_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButton1();
+	afx_msg void OnBnClickedButton2();
+	afx_msg void OnBnClickedButton3();
+	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
+};
